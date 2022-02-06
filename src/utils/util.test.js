8d0f5b7b20e12ae2038f3cd7bc56c75a1141bfb2
ofoe-fiergbor <ofoefiergbor16@gmail.com>
2022-02-06 21:12:42 +0000

    tests for sorting parameters

---
+++ src/utils/util.test.js
@@ -0,0 +1,56 @@
+const {
+  sortNameAscending,
+  sortNameDescending,
+  dateAscending,
+  dateDescending,
+} = require("./Utils");
+
+let testData = [
+  { name: "incididunt Excepteur ipsum", created: "2022-02-06T20:55:55.412647" },
+  { name: "Voluptate Veniam, Ullamco", created: "2022-02-06T20:55:55.458345" },
+  { name: "Adipiscing Aliqua", created: "2022-02-06T20:55:55.293526" },
+];
+
+test("Should sort by name in ascending order", () => {
+  expect(sortNameAscending(testData)).toEqual([
+    { name: "Adipiscing Aliqua", created: "2022-02-06T20:55:55.293526" },
+    {
+      name: "incididunt Excepteur ipsum",
+      created: "2022-02-06T20:55:55.412647",
+    },
+    {
+      name: "Voluptate Veniam, Ullamco",
+      created: "2022-02-06T20:55:55.458345",
+    },
+  ]);
+});
+
+test("Should sort by name in decending order", () => {
+  expect(sortNameDescending(testData)).toEqual([
+    {
+      name: "Voluptate Veniam, Ullamco",
+      created: "2022-02-06T20:55:55.458345",
+    },
+    {
+      name: "incididunt Excepteur ipsum",
+      created: "2022-02-06T20:55:55.412647",
+    },
+    { name: "Adipiscing Aliqua", created: "2022-02-06T20:55:55.293526" },
+  ]);
+});
+
+test("Should sort by created date in ascending order", () => {
+    expect(dateAscending(testData)).toEqual([
+        {name: "Adipiscing Aliqua", created: "2022-02-06T20:55:55.293526" },
+        {name: "incididunt Excepteur ipsum",created: "2022-02-06T20:55:55.412647"},
+        {name: "Voluptate Veniam, Ullamco", created: "2022-02-06T20:55:55.458345"},
+    ]);
+  });
+  
+  test("Should sort by created date in decending order", () => {
+    expect(dateDescending(testData)).toEqual([
+        {name: "Voluptate Veniam, Ullamco", created: "2022-02-06T20:55:55.458345"},
+        {name: "incididunt Excepteur ipsum",created: "2022-02-06T20:55:55.412647"},
+        { name: "Adipiscing Aliqua", created: "2022-02-06T20:55:55.293526" },
+    ]);
+  });
