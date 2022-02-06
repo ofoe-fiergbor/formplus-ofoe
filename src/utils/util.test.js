486ff74f03e96685243136328d776264a7916c41
ofoe-fiergbor <ofoefiergbor16@gmail.com>
2022-02-06 21:13:36 +0000

    tests for sorting parameters

--- src/utils/util.test.js
+++ src/utils/util.test.js
@@ -11,7 +11,7 @@ let testData = [
   { name: "Adipiscing Aliqua", created: "2022-02-06T20:55:55.293526" },
 ];
 
-test("Should sort by name in ascending order", () => {
+test("Should sort template array by name in ascending order", () => {
   expect(sortNameAscending(testData)).toEqual([
     { name: "Adipiscing Aliqua", created: "2022-02-06T20:55:55.293526" },
     {
@@ -25,7 +25,7 @@ test("Should sort by name in ascending order", () => {
   ]);
 });
 
-test("Should sort by name in decending order", () => {
+test("Should sort template array by name in decending order", () => {
   expect(sortNameDescending(testData)).toEqual([
     {
       name: "Voluptate Veniam, Ullamco",
@@ -39,7 +39,7 @@ test("Should sort by name in decending order", () => {
   ]);
 });
 
-test("Should sort by created date in ascending order", () => {
+test("Should sort template array by created date in ascending order", () => {
     expect(dateAscending(testData)).toEqual([
         {name: "Adipiscing Aliqua", created: "2022-02-06T20:55:55.293526" },
         {name: "incididunt Excepteur ipsum",created: "2022-02-06T20:55:55.412647"},
@@ -47,7 +47,7 @@ test("Should sort by created date in ascending order", () => {
     ]);
   });
   
-  test("Should sort by created date in decending order", () => {
+  test("Should sort template array by created date in decending order", () => {
     expect(dateDescending(testData)).toEqual([
         {name: "Voluptate Veniam, Ullamco", created: "2022-02-06T20:55:55.458345"},
         {name: "incididunt Excepteur ipsum",created: "2022-02-06T20:55:55.412647"},
